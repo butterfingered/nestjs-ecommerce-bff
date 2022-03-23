a0da64549e6f8ebd1c461f16ec9f4bd4f3864b9b
f0s01xr <felipe.soto@walmart.com>
2022-03-23 14:02:38 -0300

    migration shcema fixed

--- migrations/1648034914829-inital-schema.js
+++ migrations/1648034914829-inital-schema.js
@@ -1,15 +1,62 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+export default class initalSchema1648034914829 {
+  name = 'initalSchema1648034914829'
 
-module.exports = class initalSchema1648034914829 {
-    name = 'initalSchema1648034914829'
+  async up(queryRunner) {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'admin',
+            type: 'boolean',
+            default: 'true',
+          },
+        ],
+      }),
+    )
 
-    async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "report" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "price" integer NOT NULL)`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" varchar PRIMARY KEY NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL)`);
-    }
+    await queryRunner.createTable(
+      new Table({
+        name: 'report',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          { name: 'approved', type: 'boolean', default: 'false' },
+          { name: 'price', type: 'float' },
+          { name: 'make', type: 'varchar' },
+          { name: 'model', type: 'varchar' },
+          { name: 'year', type: 'integer' },
+          { name: 'lng', type: 'float' },
+          { name: 'lat', type: 'float' },
+          { name: 'mileage', type: 'integer' },
+          { name: 'userId', type: 'integer' },
+        ],
+      }),
+    )
+  }
 
-    async down(queryRunner) {
-        await queryRunner.query(`DROP TABLE "user"`);
-        await queryRunner.query(`DROP TABLE "report"`);
-    }
+  async down(queryRunner) {
+    await queryRunner.query(`DROP TABLE "user"`)
+    await queryRunner.query(`DROP TABLE "report"`)
+  }
 }
